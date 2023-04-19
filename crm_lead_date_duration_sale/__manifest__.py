18852c675c4ca7ee25214e86b93d06796ccca079
LuPinheiro <gerallupinheiro@gmail.com>
2023-04-19 13:29:35 +0200

    [ADD] crm_lead_date_duration_sale

---
+++ crm_lead_date_duration_sale/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "CRM Lead Date Duration Sale",
+    "summary": """
+        Links CRM event date management to Sales Order generation.
+    """,
+    "author": "Solvos",
+    "license": "AGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "CRM",
+    "website": "https://github.com/solvosci/slv-crm",
+    "depends": ["sale_crm", "crm_lead_date_duration"],
+    "data": [],
+    "installable": True,
+}
